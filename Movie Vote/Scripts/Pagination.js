29e9e2fd66f917900a708518c8ab4a301d433eac
aenen <yaroslav.golyuk@yandex.ua>
2018-05-22 18:40:58 +0300

    додав кнопку "Завантажити ще" до пейджинг-плагіну

--- Movie Vote/Scripts/Pagination.js
+++ Movie Vote/Scripts/Pagination.js
@@ -5,11 +5,17 @@
         var defaultSettings = {
             dataElementSelector: "#pageData",
             urlParameters: {},
-            beforeFunction: $.noop,
-            afterSuccessFunction: $.noop,
-            afterErrorFunction: $.noop,
-            dontLoadActiveOrDisabledPage: true,
+
+            beforeLoadPage: $.noop,
+            afterLoadPageSuccess: $.noop,
+            afterLoadPageError: $.noop,
+
             loadMoreButton: false,
+            beforeLoadMore: $.noop,
+            afterLoadMoreSuccess: $.noop,
+            afterLoadMoreError: $.noop,
+
+            dontLoadActiveOrDisabledPage: true,
             loadPageOnCreating: 0
         };
         var settings = $.extend({}, defaultSettings, options);
@@ -34,12 +40,20 @@
 
             container.find("a[data-page='" + currentPage + "']").parent("li").addClass("active");
             $(thisElement).append(container);
+
+            if (settings.loadMoreButton) {
+                $("<button/>", {
+                    class: "btn btn-lg btn-primary",
+                    id: "load-more",
+                    text: "Завантажити ще"
+                }).on("click", loadMoreClick).insertBefore(thisElement);
+            }
         }
 
         /**
          * @summary Натиснення на елемент сторінки (ul.pagination li a.page).
          */
-        function pageClick() {
+        function pageClick(e) {
             if (settings.dontLoadActiveOrDisabledPage) {
                 if ($(this).parent().hasClass("active") || $(this).parent().hasClass("disabled")) {
                     return false;
@@ -49,10 +63,46 @@
             var page = $(this).data("page");
             var self = this;
 
+            settings.beforeLoadPage();
             loadPage(page, false, function () {
                 $("ul.pagination li.active").removeClass("active");
                 $(self).parent().addClass("active");
-            });
+
+                if (settings.loadMoreButton) {
+                    if (page == totalPages) {
+                        $("#load-more").hide();
+                    } else {
+                        $("#load-more").show();
+                    }
+                }
+
+                settings.afterLoadPageSuccess();
+            }, settings.afterLoadPageError);
+        }
+
+        /**
+         * @summary Натиснення на кнопку "Завантажити ще" (#load-more).
+         */
+        function loadMoreClick(e) {
+            if (settings.dontLoadActiveOrDisabledPage) {
+                if ($(this).hasClass("active") || $(this).hasClass("disabled")) {
+                    return false;
+                }
+            }
+
+            var self = this;
+            var page = $("ul.pagination > li.active > a").last().data("page") + 1;
+
+            settings.beforeLoadMore();
+            loadPage(page, true, function () {
+                $("ul.pagination > li.active").next().addClass("active");
+
+                if (page == totalPages) {
+                    $(self).hide();
+                }
+
+                settings.afterLoadMoreSuccess();
+            }, settings.afterLoadMoreError);
         }
 
         /**
@@ -61,10 +111,9 @@
          * @param {number}   page      Номер сторінки, яку необхідно завантажити.
          * @param {boolean}  append    false - очищає контейнер перед вставкою; true - додає данні в кінець контейнеру.
          * @param {function} onSuccess Функція, яка виконується при успішному завантаженні даних.
+         * @param {function} onError   Функція, яка виконується при не успішному завантаженні даних.
          */
-        function loadPage(page, append, onSuccess = $.noop) {
-            settings.beforeFunction();
-            
+        function loadPage(page, append, onSuccess = $.noop, onError = $.noop) {            
             var params = $.extend({ page: page }, settings.urlParameters);
             $.ajax({
                 type: "GET",
@@ -76,12 +125,8 @@
                     $(settings.dataElementSelector).append(dataHtml);
 
                     onSuccess();
-
-                    settings.afterSuccessFunction();
                 },
-                error: function () {
-                    settings.afterErrorFunction();
-                }
+                error: () => onError()
             });
         }
     };
